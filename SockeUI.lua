a3db3336eefa65e03be76e2b86161ac220892829
Simon Stockhause <gamingsockee@gmail.com>
2020-10-06 23:03:47 +0200

    comment in code

--- SockeUI.lua
+++ SockeUI.lua
@@ -1,13 +1,13 @@
-Copyright (C) 2020 Simon Stockhause
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
+-- Copyright (C) 2020 Simon Stockhause
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
 
 SockeUI = {
     fontSize = 10, -- font size for default table
